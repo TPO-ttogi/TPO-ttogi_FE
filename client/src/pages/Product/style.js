1743e9f1861e4638115ad815acea0959dd0ce69f
jeongeun <wjddms9921@gmail.com>
2023-09-23 02:42:38 +0900

    ADD : font-face, Product 페이지 퍼블리싱

--- client/src/pages/Product/style.js
+++ client/src/pages/Product/style.js
@@ -28,10 +28,52 @@ export const Keyword = styled.div`
 `;
 
 export const Main = styled.div`
+  width: 100%;
   display: flex;
-  justify-content: center;
+  align-items: center;
   margin-top: 34px;
+  flex-direction: column;
   .title {
-    font-family: "jejudoldam";
+    font-family: "EF_jejudoldam";
+    font-size: 24px;
   }
+  .subTitle {
+    font-family: "HakgyoansimWoojuR";
+    font-size: 12px;
+    margin-top: 6px;
+    margin-bottom: 28px;
+  }
+  .leftTextBox {
+    width: 100%;
+    display: flex;
+    justify-content: start;
+  }
+`;
+
+export const Detail = styled.div`
+  width: fit-content;
+  height: fit-content;
+  border: 0.5px solid black;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 30px;
+  div {
+    padding: 10px;
+    height: 33px;
+    line-height: 33px;
+    font-family: "Inter";
+    border-top: 0.5px solid black;
+  }
+`;
+
+export const BotText = styled.div`
+  border: 0.5px solid black;
+  border-radius: 10px;
+  max-width: 200px;
+  padding: 10px 20px;
+  box-sizing: border-box;
+  font-family: "HakgyoansimWoojuR";
+  font-size: 12px;
+  margin-bottom: 10px;
 `;
