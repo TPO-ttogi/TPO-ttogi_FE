8c8c8d27450b2e01a08c4f1ade2da99a49bf748f
VanesaK <minitea@sookmyung.ac.kr>
2023-09-23 02:06:20 +0900

    FIX: 홈 화면 생성 중(emotion 해결)

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { Circle } from './style.js';
+
 
 const Home = () => {
-    return (
-      <div>
-        <h1>홈</h1>
-        <p>가장 먼저 보여지는 페이지입니다.</p>
-      </div>
-    );
-  };
-  
+  return (<div>
+    <Circle>dd</Circle>
+    <img src="../../../public/assets/logo/TPO_logo.svg"/>
+    <img src="../../../public/assets/image/ottogi_main_character.svg"/>
+    
+    <div style={{paddingLeft:"20px", paddingRight:"20px",}}>
+      <div>🏠 comfort food</div>
+      <div>🥨 snack</div>
+      <div>🏫 after school</div>
+      <div>🌃 late-night meal</div>
+      <div>💊 sick</div>
+      <div>🎂 birthday</div>
+      <div>🥣 grab a bite</div>
+      <div>🎎 Korean holiday</div>
+      <div>🎉 housewarming</div>
+      <div>🍺 hangover food</div>
+      <div>🧓🏻 with elders</div>
+      <div>🧒🏻 with children</div>
+      <div>🍜 sick of rice</div>
+      <div>🍺 going out to drink</div>
+      <div>🌋 hot weather</div>
+      <div>❄️ cold weather</div>
+    </div>
+
+      <div>TPO!ttogi</div>
+  </div>
+
+  );
+};
+
 export default Home;
