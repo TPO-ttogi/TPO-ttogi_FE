498b10863d24812fdcf8bb1019c03718ee58d1e0
VanesaK <minitea@sookmyung.ac.kr>
2023-09-23 03:35:34 +0900

    EDIT: 홈 화면 퍼블리싱 완성(애니메이션 제외)

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,33 +1,38 @@
 import React from "react";
-import { Circle } from './style.js';
+import { CircleArea, Circle1, Circle2, Circle3, ImageArea, KeywordCell, FooterText} from './style.js';
 
 
 const Home = () => {
-  return (<div>
-    <Circle>dd</Circle>
-    <img src="../../../public/assets/logo/TPO_logo.svg"/>
-    <img src="../../../public/assets/image/ottogi_main_character.svg"/>
-    
-    <div style={{paddingLeft:"20px", paddingRight:"20px",}}>
-      <div>🏠 comfort food</div>
-      <div>🥨 snack</div>
-      <div>🏫 after school</div>
-      <div>🌃 late-night meal</div>
-      <div>💊 sick</div>
-      <div>🎂 birthday</div>
-      <div>🥣 grab a bite</div>
-      <div>🎎 Korean holiday</div>
-      <div>🎉 housewarming</div>
-      <div>🍺 hangover food</div>
-      <div>🧓🏻 with elders</div>
-      <div>🧒🏻 with children</div>
-      <div>🍜 sick of rice</div>
-      <div>🍺 going out to drink</div>
-      <div>🌋 hot weather</div>
-      <div>❄️ cold weather</div>
+  return (
+  <div style={{ width: "100%" }}>
+    <img style={{ width:"120%", marginLeft:"-5%",marginTop:"-5%",marginRight:"-5%",zIndex:1,}} src="/assets/image/main_yellow_circles.svg" />
+
+    <div style={{ width: "100%", display: "flex", flexDirection: "row", flexWrap: "wrap", marginBottom:"36px", paddingLeft: "20px", paddingRight: "20px", textAlign: "center", justifyContent: "center" }}>
+      <img style={{ width: "64.8%", marginRight: "auto", marginLeft: "auto" }} src="/assets/logo/TPO_logo.svg" />
+      <img style={{ width: "64.8%", marginRight: "auto", marginLeft: "auto" }} src="/assets/image/ottogi_main_character.svg" />
     </div>
 
-      <div>TPO!ttogi</div>
+
+    <div style={{ width: "100%", display: "flex", flexDirection: "row", flexWrap: "wrap", paddingLeft: "20px", paddingRight: "20px", textAlign: "center", justifyContent: "center" }}>
+      <KeywordCell>🏠 comfort food</KeywordCell>
+      <KeywordCell>🥨 snack</KeywordCell>
+      <KeywordCell>🏫 after school</KeywordCell>
+      <KeywordCell>🌃 late-night meal</KeywordCell>
+      <KeywordCell>💊 sick</KeywordCell>
+      <KeywordCell>🎂 birthday</KeywordCell>
+      <KeywordCell>🥣 grab a bite</KeywordCell>
+      <KeywordCell>🎎 Korean holiday</KeywordCell>
+      <KeywordCell>🎉 housewarming</KeywordCell>
+      <KeywordCell>🍺 hangover food</KeywordCell>
+      <KeywordCell>🧓🏻 with elders</KeywordCell>
+      <KeywordCell>🧒🏻 with children</KeywordCell>
+      <KeywordCell>🍜 sick of rice</KeywordCell>
+      <KeywordCell>🍺 going out to drink</KeywordCell>
+      <KeywordCell>🌋 hot weather</KeywordCell>
+      <KeywordCell>❄️ cold weather</KeywordCell>
+    </div>
+
+    <FooterText>TPO!ttogi</FooterText>
   </div>
 
   );
