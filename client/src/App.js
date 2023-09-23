eeed07420fe45c9d155233980511ec21449b652e
jeongeun <wjddms9921@gmail.com>
2023-09-23 09:30:00 +0900

    ADD : GEN AI API 연동

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Home from "./pages/Home/index.tsx"
-import ProductList from "./pages/ProductList/index.tsx"
-import Product from "./pages/Product/index.tsx"
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/index.tsx";
+import ProductList from "./pages/ProductList/index.tsx";
+import Product from "./pages/Product/index.jsx";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/productList" element={<ProductList/>}/>
+      <Route path="/productList" element={<ProductList />} />
       <Route path="/product" element={<Product />} />
     </Routes>
-
   );
 }
 
---
+++ client/src/pages/Product/index.jsx
@@ -0,0 +1,129 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+import {
+  Container,
+  Header,
+  Keyword,
+  Main,
+  Detail,
+  Message,
+  LeftTextBox,
+  Graph,
+  RightTextBox,
+} from "./style.js";
+
+const Product = () => {
+  const navigate = useNavigate();
+  const [genAIAnswer, setGenAIAnswer] = useState("...");
+
+  const getGenAIAnswer = async () => {
+    const response = await axios.post(
+      "https://api.openai.com/v1/chat/completions",
+      {
+        messages: [{ role: "user", content: "What is Tteok bokki" }],
+        model: "gpt-3.5-turbo",
+      },
+      {
+        headers: {
+          Authorization: "Bearer " + process.env.REACT_APP_GEN_AI_KEY,
+          "Content-Type": "application/json",
+        },
+      }
+    );
+
+    setGenAIAnswer(response?.data.choices[0]?.message?.content);
+  };
+
+  useEffect(() => {
+    getGenAIAnswer();
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <Header>
+          <img
+            src="/assets/icon/back.svg"
+            width={28}
+            height={17}
+            onClick={() => navigate("/productList")}
+          />
+          <img
+            src="/assets/icon/home.svg"
+            width={28}
+            height={31}
+            onClick={() => navigate("/")}
+          />
+        </Header>
+        <Keyword>after school</Keyword>
+        <Main>
+          <div className="title">Tteok bokki</div>
+          <div className="subTitle">(떡볶이)</div>
+          <Detail>
+            <img
+              src="http://www.ottogi.co.kr/pds/product/2021-07-28_60726959[9].jpg"
+              width={147}
+              height={147}
+            />
+            <div className="name">SNACK SHOP RICE TTEOK-BOKKI</div>
+          </Detail>
+          <LeftTextBox>
+            <img src="/assets/image/bot_profile.svg" width={39} height={36} />
+            <Message>우리 떡볶이 먹으러갈래?</Message>
+          </LeftTextBox>
+          <RightTextBox style={{ animationDelay: "1s" }}>
+            <Message>떡볶이가 뭐야?</Message>
+          </RightTextBox>
+          <LeftTextBox
+            style={{ animationDelay: "2s" }}
+            className="signal"
+            start={genAIAnswer !== "..."}
+          >
+            <img src="/assets/image/bot_profile.svg" width={39} height={36} />
+            <Message>{genAIAnswer}</Message>
+          </LeftTextBox>
+          <RightTextBox
+            className="signal"
+            style={{ animationDelay: "3s" }}
+            start={genAIAnswer !== "..."}
+          >
+            <Message>그거 맵지 않아?</Message>
+          </RightTextBox>
+          <LeftTextBox
+            style={{ animationDelay: "4s" }}
+            className="big signal"
+            start={genAIAnswer !== "..."}
+          >
+            <img src="/assets/image/bot_profile.svg" width={39} height={36} />
+            <Message>
+              <div>한국인 n명 중에 n명이 맵다고 했어.</div>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  marginTop: "5px",
+                }}
+              >
+                <Graph color="red" percent={90} />
+                <div>90%</div>
+              </div>
+            </Message>
+          </LeftTextBox>
+          <LeftTextBox
+            className="signal"
+            start={genAIAnswer !== "..."}
+            style={{ animationDelay: "5s" }}
+          >
+            <img src="/assets/image/bot_profile.svg" width={39} height={36} />
+            <Message>우리 떡볶이 먹으러갈래?</Message>
+          </LeftTextBox>
+        </Main>
+      </Container>
+      <img src="/assets/image/background_bottom.svg" width={"100%"} />
+    </>
+  );
+};
+
+export default Product;
